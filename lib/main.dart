06464080c65e31451d299a68b43e50fa12368a07
Vathan Rahman <vathanaja14@gmail.com>
2021-01-26 10:03:27 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:eticket/services/services.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(
+                child: Text("Sign Up"),
+                onPressed: () async {
+                  
+                }
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,3 @@
+part of "shared.dart";
+
+String apiKey = "61ec665a3b3f08b0ff177ad9d0927324";
